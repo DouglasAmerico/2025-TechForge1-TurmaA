3eaf1e931471045eaeb68c8b6e6cef65736ca434
Douglas Américo <douglas@likesistemas.com.br>
2025-03-11 20:54:09 -0300

    Exericios do slide da aula 2

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let nome = "Douglas Américo";
+const leitura = require("prompt-sync")();
 
-console.log(nome);
+let nome = leitura("Informe o seu nome: ");
+let corFavorita = leitura("Informe sua cor favorita: ");
+let filmeFavorito = leitura("Informe seu filme favorito: ");
+let cidade = leitura("Informe o nome da cidade que você mora: ");
+let animalEstimacao = leitura("Informe o nome do seu animal de estimação: ");
+console.log("Bem vindo ao meu sistema "+nome+", que tem a cor favorita "+corFavorita);
+console.log("Quem tem o filme favorito chamado "+filmeFavorito);
+console.log("Que mora na cidade "+cidade);
+console.log("Que tem o animal de estimação chamado: "+animalEstimacao);
+
+
 
